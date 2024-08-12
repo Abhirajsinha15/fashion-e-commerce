c9ec8a1a549a2c04de16a1fc2301fb000bf045e8
Abhiraj <abhirajsinha15@gmail.com>
2024-08-12 14:00:34 +0530

    Updated typography, cards, and mixins, and made adjustments to slider.js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fashion-e-commerce
 fashion e-commerce site tools- Html5, JQuery, bootstrap,Sass
+
+
+
--- js/slider.js
+++ js/slider.js
@@ -41,3 +41,45 @@
     });
 
     });
+
+    $(document).ready(function() {
+        $('.testimonial-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        arrows:true,
+        prevArrow:`<i class="fa fa-arrow-left left-arrow arrows" aria-hidden="true"></i>`,
+        nextArrow:`<i class="fa fa-arrow-right right-arrow arrows" aria-hidden="true"></i>`,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+        ]
+    });
+
+    });
